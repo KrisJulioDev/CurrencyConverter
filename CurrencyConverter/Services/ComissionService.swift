06205e2b7372751413b2ae07d618e88e816c39a5
Kris Julio <krisgamedev@gmail.com>
2022-10-10 21:15:52 +0800

    Move comission logic to comission service

--- CurrencyConverter/Services/ComissionService.swift
+++ CurrencyConverter/Services/ComissionService.swift
@@ -7,7 +7,50 @@
 
 import Foundation
 
+enum PromoType: Equatable {
+    case freeTransaction(Int)
+    case minAmount(Int)
+    case interval(Int)
+    case none
+}
+
 class ComissionService: DecodingServiceProtocol {
+     
+    func availablePromo(comission: Comission,
+                        userWallet: UserWallet,
+                        currencyInSelect: String?,
+                        currencyAmount: Double) -> PromoType {
+        var promoType: PromoType = .none
+        
+        /// check promo if the user is eligible for free transactions
+        comission.promos.forEach { promo in
+            if promo.type == "free_transaction" {
+                if userWallet.transactionsFulfilled < promo.value {
+                    promoType = .freeTransaction(promo.value)
+                    return
+                }
+            }
+            
+            if promo.type == "intervals" {
+                if userWallet.transactionsFulfilled > 0 &&
+                    /// add + 1, fulfilled + this transaction check
+                    (userWallet.transactionsFulfilled + 1) % promo.value == 0 {
+                    promoType = .interval(promo.value)
+                    return
+                }
+            }
+            
+            if promo.type == "amount_minimum", currencyInSelect == promo.currency {
+                if currencyAmount >= Double(promo.value) {
+                    promoType = .minAmount(promo.value)
+                    return
+                }
+            }
+        }
+        
+        return promoType
+    }
+    
     /// fetch comission from json, throw error serviceNotAvailable if fails
     func fetchServiceData<T: Codable>() throws -> T {
         guard let jsonFile = Bundle.main.url(forResource: "commision_rules",
@@ -23,4 +66,4 @@ class ComissionService: DecodingServiceProtocol {
             throw ExchangeError.serviceNotAvailable
         }
     }
-} 
+}
